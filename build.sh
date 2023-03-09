84148cba7e16031c6a772f6bb921338043146da7
GOPAVARAM SAI MADHUREE <82037879+MADHUREE2801@users.noreply.github.com>
2023-03-09 14:07:37 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -5,5 +5,5 @@ set -o errexit
 pip install --upgrade pip
 pip install -r requirements.txt
 python3 -m pip install --upgrade pip
-pip3 install psycopg2 gunicorn 
-pip3 freeze > requirements.txt
+pip install gunicorn 
+pip freeze > requirements.txt
